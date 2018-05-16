0197cd3b827a07ffc1b1b356d66cc4c40663e8a9
Sevak Mardirosian <smardirosians@gmail.com>
2018-05-16 12:56:44 +0200

    fix in slashes

--- criteo/get1tbset.sh
+++ criteo/get1tbset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-1tb_path=data/criteo
-1tb_actual_path=data/vw/1TB
+1tb_path=/data/criteo
+1tb_actual_path=/data/vw/1TB
 
 for i in {0..23}; do
     curl http://azuremlsampleexperiments.blob.core.windows.net/criteo/day_${i}.gz | \
